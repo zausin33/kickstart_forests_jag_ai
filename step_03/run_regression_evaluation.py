1570a21240367cc2a041e4d9f38d9284e8e58b96
Anthony Akpenyi <owuzoniwe@gmail.com>
2024-02-18 08:30:28 +0100

    adds starter class for step03

---
+++ step_03/run_regression_evaluation.py
@@ -0,0 +1,286 @@
+import numpy as np
+import optuna
+import config
+import pandas as pd
+from lightgbm import LGBMRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+from sklearn.decomposition import PCA
+
+from typing import Tuple, Optional
+import warnings
+warnings.filterwarnings('ignore', category=UserWarning)
+
+LEAF_AREA_INDEX = 'lai'
+LEAF_ID = 'id'
+
+class Dataset:
+    def __init__(self, num_samples: Optional[int] = None, random_seed: int = 42,
+                 data_filename: str = 'RtmSimulation_kickstart.csv'):
+        """
+        :param num_samples: the number of samples to draw from the data frame; if None, use all samples
+        :param random_seed: the random seed to use when sampling data points
+        :param data_filename: the filename of file containing the dataset
+        """
+        self.num_samples = num_samples
+        self.random_seed = random_seed
+        self.data_filename = data_filename
+
+    def load_data_frame(self) -> pd.DataFrame:
+        """
+        :return: the full data frame for this dataset (including the class column)
+        """
+        df = pd.read_csv(config.csv_data_path(self.data_filename))
+        if self.num_samples is not None:
+            df = df.sample(self.num_samples, random_state=self.random_seed)
+        return df
+
+    def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
+        """
+        :return: a pair (X, y) where X is the data frame containing all attributes and y is the corresponding
+        series of class values
+        """
+        df = self.load_data_frame()
+        return df.drop(columns=[LEAF_AREA_INDEX], axis=1), df[LEAF_AREA_INDEX]
+
+class RegressionEvaluator:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    data = Dataset()
+
+    # define target and features
+    X, y = data.load_xy()
+    print('Xshape: \n{}'.format(X.shape))
+
+    """
+        Train-Test Split
+        Train-test split is performed with a 60-20-20 ratio.
+        Feature engineering steps are applied separately to the train test and validation sets to avoid data leakage.
+        Shuffle is left to True to ensure that all sets have the same distribution.
+        To check this, we plot the distribution of the target variable.
+    """
+
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
+    X_test, X_val, y_test, y_val = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+    # Run some plots
+    sns.kdeplot(y_train, label='y_train')
+    sns.kdeplot(y_val, label='y_val')
+    sns.kdeplot(y_test, label='y_test')
+    plt.legend()
+    plt.show()
+
+    """
+    Feature Engineering
+        We fill missing values with the median of each column.
+        We perform PCA on the wavelength features to reduce the dimensionality. We retain 95% of the variance.
+        Categories are encoded with OrdinalEncoder and then passed to LightGBM as categorical features. LightGBM will handle the encoding internally.
+        Numerical features are scaled with StandardScaler.
+    Explaining Feature Selection
+        We keep the treeSpecies and wetness features and only perform dimensionality reduction on the wavelength features.
+        This is because the wavelength features are highly correlated and we want to reduce the dimensionality of the dataset.
+    """
+
+    # Identify categorical and numerical columns
+    categorical_cols = [col for col in X.columns if X[col].dtype == 'object']
+    numerical_cols = [col for col in X.columns if col not in categorical_cols + [LEAF_ID, LEAF_AREA_INDEX]]
+
+    # Fill missing values
+    X_train[numerical_cols] = X_train[numerical_cols].fillna(X_train[numerical_cols].median())
+    X_val[numerical_cols] = X_val[numerical_cols].fillna(X_train[numerical_cols].median())
+    X_test[numerical_cols] = X_test[numerical_cols].fillna(X_train[numerical_cols].median())
+
+    # feature selection and expansion methods
+    pca = PCA(n_components=0.95)  # Retain 95% of variance
+    X_train_pca = pca.fit_transform(X_train.iloc[:, 2:])
+    X_val_pca = pca.transform(X_val.iloc[:, 2:])
+    X_test_pca = pca.transform(X_test.iloc[:, 2:])
+
+    # Convert the PCA results into a DataFrame
+    X_train_pca_df = pd.DataFrame(X_train_pca, columns=[f'PCA_{i}' for i in range(X_train_pca.shape[1])],
+                                  index=X_train.index)
+    X_val_pca_df = pd.DataFrame(X_val_pca, columns=[f'PCA_{i}' for i in range(X_val_pca.shape[1])], index=X_val.index)
+    X_test_pca_df = pd.DataFrame(X_test_pca, columns=[f'PCA_{i}' for i in range(X_test_pca.shape[1])],
+                                 index=X_test.index)
+
+    X_train = pd.concat([X_train.iloc[:, :2], X_train_pca_df], axis=1)
+    X_val = pd.concat([X_val.iloc[:, :2], X_val_pca_df], axis=1)
+    X_test = pd.concat([X_test.iloc[:, :2], X_test_pca_df], axis=1)
+
+    # Identify new categorical and numerical columns
+    categorical_cols = [col for col in X_train.columns if X_train[col].dtype == 'object']
+    numerical_cols = [col for col in X_train.columns if col not in categorical_cols + [LEAF_ID, LEAF_AREA_INDEX]]
+
+    print(X_train)
+
+    # encode categorical features
+    ordinal_encoder = OrdinalEncoder()
+    X_train[categorical_cols] = ordinal_encoder.fit_transform(X_train[categorical_cols])
+    X_val[categorical_cols] = ordinal_encoder.transform(X_val[categorical_cols])
+    X_test[categorical_cols] = ordinal_encoder.transform(X_test[categorical_cols])
+
+    # scale numerical features
+    scl = StandardScaler()
+    X_train[numerical_cols] = scl.fit_transform(X_train[numerical_cols])
+    X_val[numerical_cols] = scl.transform(X_val[numerical_cols])
+    X_test[numerical_cols] = scl.transform(X_test[numerical_cols])
+
+    # modelling
+        # lightGBM
+    gbm_model = LGBMRegressor(random_state=42)
+    gbm_model.fit(X_train, y_train, categorical_feature=set(categorical_cols))
+
+    y_pred_train = gbm_model.predict(X_train)
+    mse = mean_squared_error(y_train, y_pred_train)
+
+    print("Train MSE:", mse, "Train r2:", r2_score(y_train, y_pred_train))
+
+    y_pred_val = gbm_model.predict(X_val)
+    mse = mean_squared_error(y_val, y_pred_val)
+
+    print("Validation MSE:", mse, "Validation r2:", r2_score(y_val, y_pred_val))
+
+    y_pred_test = gbm_model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred_test)
+
+    print("Test MSE:", mse, "Test r2:", r2_score(y_test, y_pred_test))
+
+    # hyperparameters optimization
+    def param_bounds(trial: optuna.Trial):
+        return {
+            # Sample an integer between 10 and 100
+            "n_estimators": trial.suggest_categorical("n_estimators", [50, 100, 200, 300, 400, 500, 700, 1000]),
+            "num_leaves": trial.suggest_int("num_leaves", 10, 100),
+            "max_depth": trial.suggest_int("max_depth", 10, 100),
+            # Sample a categorical value from the list provided
+            "objective": trial.suggest_categorical(
+                "objective", ["regression", "regression_l1", "huber"]
+            ),
+            "random_state": [42],
+            # Sample from a uniform distribution between 0.3 and 1.0
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.3, 1.0),
+            # Sample from a uniform distribution between 0 and 10
+            "reg_alpha": trial.suggest_float("reg_alpha", 0, 100),
+            # Sample from a uniform distribution between 0 and 10
+            "reg_lambda": trial.suggest_float("reg_lambda", 0, 100),
+        }
+
+    def objective(trial: optuna.Trial):
+        gbm_model = LGBMRegressor()
+        params = param_bounds(trial)
+        gbm_model.set_params(**params)
+
+        gbm_model.fit(X_train, y_train, categorical_feature=set(categorical_cols))
+
+        return gbm_model.score(X_val, y_val)
+
+
+    sampler = optuna.samplers.TPESampler(n_startup_trials=10, seed=42)
+    # Create a study
+    study = optuna.create_study(direction="maximize", sampler=sampler)
+    # Start the optimization run
+    study.optimize(objective, n_trials=50, show_progress_bar=True)
+
+    fig = optuna.visualization.plot_param_importances(study)
+    fig.show()
+
+    bo_search_trials = study.trials_dataframe()
+    best_params = study.best_params
+    best_score = study.best_value
+    print(bo_search_trials.sort_values("value").head())
+    print(f"Best parameters: {best_params}")
+    print(f"Best score: {best_score}")
+
+    # train with best parameters
+    gbm_model = LGBMRegressor(random_state=42, **best_params)
+    start = time.time()
+    gbm_model.fit(X_train, y_train, categorical_feature=set(categorical_cols))
+    end = time.time()
+    print(f"Training time: {end - start} seconds")
+
+    start = time.time()
+    y_pred_train = gbm_model.predict(X_train)
+    end = time.time()
+    mse = mean_squared_error(y_train, y_pred_train)
+    print("Train MSE:", mse, "Train r2:", r2_score(y_train, y_pred_train), f"Prediction time: {end - start} seconds")
+
+    start = time.time()
+    y_pred_val = gbm_model.predict(X_val)
+    end = time.time()
+    mse = mean_squared_error(y_val, y_pred_val)
+    print("Validation MSE:", mse, "Validation r2:", r2_score(y_val, y_pred_val),
+          f"Prediction time: {end - start} seconds")
+
+    start = time.time()
+    y_pred_test = gbm_model.predict(X_test)
+    end = time.time()
+    mse = mean_squared_error(y_test, y_pred_test)
+    print("Test MSE:", mse, "Test r2:", r2_score(y_test, y_pred_test), f"Prediction time: {end - start} seconds")
+
+    """
+    Cross validation
+    We concatenate the train and validation sets to perform cross-validation on the whole dataset.
+    """
+    gbm_model = LGBMRegressor(random_state=42, **best_params)
+
+    X_train_cross_val, y_train_cross_val = pd.concat([X_train, X_val]), pd.concat([y_train, y_val])
+
+    # Define the cross-validation strategy
+    cv_strategy = KFold(n_splits=5, shuffle=True, random_state=42)
+
+    # Perform cross-validation for R² score
+    r2_scores = cross_val_score(gbm_model, X_train_cross_val, y_train_cross_val, cv=cv_strategy,
+                                fit_params={'categorical_feature': set(categorical_cols)})
+
+    # Calculate mean and standard deviation of R² scores
+    r2_mean = np.mean(r2_scores)
+    r2_std = np.std(r2_scores)
+
+    # Perform cross-validation for MSE
+    mse_scores = cross_val_score(gbm_model, X_train, y_train, scoring='neg_mean_squared_error', cv=cv_strategy,
+                                 fit_params={'categorical_feature': set(categorical_cols)})
+
+    # Calculate mean and standard deviation of MSE scores
+    mse_mean = np.mean(-mse_scores)  # Negate the scores back to positive
+    mse_std = np.std(-mse_scores)  # Negate the scores back to positive
+
+    print(f"Mean cross-validation R²: {r2_mean:.3f} +/- {r2_std:.3f}")
+    print(f"Mean cross-validation MSE: {mse_mean:.3f} +/- {mse_std:.3f}")
+
+    """
+    Final evaluation
+    We train the model on the whole dataset and evaluate it on the test set, to get the final performance metrics
+    """
+    gbm_model.fit(X_train_cross_val, y_train_cross_val, categorical_feature=set(categorical_cols))
+    y_pred_test = gbm_model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred_test)
+
+    print("Test MSE:", mse, "Test r2:", r2_score(y_test, y_pred_test))
+
+    """
+    Explainability
+    Gradient boosting models have a feature_importances_ attribute that can be used to get the relative importance of 
+    each feature. When using PCA, the interpretability is more limited, as the features are linear 
+    combinations of the original features.
+    """
+    feat_df = pd.DataFrame(
+        {
+            "feature": X_train.columns,
+            "importance": gbm_model.feature_importances_.ravel(),
+        }
+    )
+
+    feat_df["_abs_imp"] = np.abs(feat_df.importance)
+    feat_df = feat_df.sort_values("_abs_imp", ascending=False).drop(
+        columns="_abs_imp"
+    )
+
+    feat_df = feat_df.sort_values(by="importance", ascending=False).head(15)
+    feat_df.plot(x="feature", y="importance", kind="bar", color="blue", )
