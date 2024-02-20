a5a043aab838432a12a0915c05344b797a4b5c07
Jonas Zausinger <jonas.zausinger1@gmail.com>
2024-02-20 08:59:25 +0100

    Finished step 8

--- step_05_08/run_regression_evaluation.py
+++ step_05_08/run_regression_evaluation.py
@@ -1,16 +1,25 @@
+import os
 import warnings
 
 from sensai.evaluation import RegressionModelEvaluation, RegressionEvaluatorParams
 from sensai.util import logging
+from sensai.util.io import ResultWriter
+from sensai.util.logging import datetime_tag
 
-from step_05_07.src.data import Dataset
-from step_05_07.src.model_factory import ModelFactory
+from step_05_08.src.data import Dataset
+from step_05_08.src.model_factory import ModelFactory
 
 warnings.filterwarnings('ignore', category=UserWarning)
 
 def main():
     # define & load dataset
     dataset = Dataset()
+
+    experiment_name = f"lai_regression_{dataset.tag()}"
+    run_id = datetime_tag()
+    result_writer = ResultWriter(os.path.join("results", experiment_name, run_id))
+    logging.add_file_logger(result_writer.path("log.txt"))
+
     io_data = dataset.load_io_data()
 
     # define models to be evaluated
@@ -23,7 +32,7 @@ def main():
 
     # use a high-level utility class for evaluating the models based on these parameters
     ev = RegressionModelEvaluation(io_data, evaluator_params=evaluator_params)
-    ev.compare_models(models, fit_models=True)
+    ev.compare_models(models, fit_models=True, result_writer=result_writer)
 
 if __name__ == '__main__':
     logging.run_main(main)
