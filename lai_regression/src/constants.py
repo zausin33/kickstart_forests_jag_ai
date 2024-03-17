1bb74e7421e1548cc34b065bcf4821d64e2672eb
Jonas Zausinger <jonas.zausinger1@gmail.com>
2024-03-17 12:09:47 +0100

    added data versioning with lakefs

--- lai_regression/src/constants.py
+++ lai_regression/src/constants.py
@@ -1,23 +1,4 @@
-# DATA_BASE_DIR = "data"
-# MODEL_BASE_DIR = "model"
-
-# AMES_HOUSING_DATA_SET_URL = "http://jse.amstat.org/v19n3/decock/AmesHousing.txt"
-# AMES_HOUSING_DATA_SET_SEPARATOR = "\t"
-
-# RANDOM_STATE = 42
-
-# SELECTED_FEATURES = {
-#     "nominal": ["ms_zoning", "lot_shape", "land_contour"],
-#     "ordinal": ["land_slope", "overall_qual", "overall_cond"],
-#     "numerical": ["lot_frontage", "lot_area", "mas_vnr_area"],
-# }
-
-# TARGET = "sale_price"
-
 MLFLOW_TRACKING_URL = "https://team3-mlflow-be2bc93-k4avrf3gmq-ey.a.run.app" #"http://localhost:5000"
 MLFLOW_USERNAME = 'admin'
 MLFLOW_PASSWORD = 'OlFhkMomho8UgHey'
 MLFLOW_EXPERIMENT = "lai_regression"
-
-# LAKEFS_REPOSITORY = "ai-kickstart"
-# LAKEFS_BRANCH = "main"
